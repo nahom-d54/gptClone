64e19efbdf49c4a35669cd9f5125d3d87c740a24
Nahom d <nahom.d54@gmail.com>
2024-11-25 17:24:15 +0300

    refactor: rename project and update API description; remove Redis config and adjust routes

--- src/config/redis.js
+++
@@ -1,8 +0,0 @@
-const { default: Redis } = require("ioredis");
-
-const initializeRedis = async () => {
-  const client = new Redis(process.env.REDIS_URL);
-  return client;
-};
-
-module.exports = initializeRedis;
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,27 +1,17 @@
 const express = require("express");
-const subscriptionRouter = require("../routes/subscription");
-const subscriptionTypesRouter = require("../routes/subscriptionTypes");
+
 const userRouter = require("../routes/user");
-const rateRouter = require("../routes/rates");
+const chatRouter = require("../routes/chat");
+
 const {
   checkIfAdmin,
   checkIfAuthenticated,
 } = require("../controllers/middlewares");
 const { authenticateJWT } = require("../config/passportConfig");
-const asyncHandler = require("../utils/asyncHandler");
-const rateController = require("../controllers/rates");
 
 const router = express.Router();
 
-router.use("/subscription", authenticateJWT, subscriptionRouter);
-router.use(
-  "/subscription-types",
-  authenticateJWT,
-  checkIfAdmin,
-  subscriptionTypesRouter
-);
-router.use("/rates", checkIfAuthenticated, rateRouter);
+router.use("/chat", authenticateJWT, chatRouter);
 router.use("/user", userRouter);
-router.get("/cron", asyncHandler(rateController.updateExchangeRate));
 
 module.exports = router;
