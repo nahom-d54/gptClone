892041a01891e2adef412c31e42b8a3024e6d471
Nahom d <nahom.d54@gmail.com>
2024-12-04 12:55:11 +0300

    refactor: update comparePassword method to use async/await and improve readability

--- src/models/User/methods.js
+++ src/models/User/methods.js
@@ -1,31 +1,33 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-  /**
-   * Compare a candidate password to the user's password.
-   *
-   * @param {string} candidatePassword - The password to compare.
-   * @returns {Promise<boolean>} - Whether the password is valid.
-   */
-const comparePassword = function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+/**
+ * Compare a candidate password to the user's password.
+ *
+ * @param {string} candidatePassword - The password to compare.
+ * @returns {Promise<boolean>} - Whether the password is valid.
+ */
+const comparePassword = async function (candidatePassword) {
+  const match = await bcrypt.compare(candidatePassword, this.password);
+
+  console.log(candidatePassword, this.password, match);
+  return await bcrypt.compare(candidatePassword, this.password);
 };
 
 const changePassword = async function (previousPassword, newPassword) {
-    if (!this.comparePassword(previousPassword)) {
-        throw new Error('Invalid password');
-    }
-    this.password = newPassword
-    return await this.save()
-}
-
+  if (!this.comparePassword(previousPassword)) {
+    throw new Error("Invalid password");
+  }
+  this.password = newPassword;
+  return await this.save();
+};
 
 const cleanUser = function () {
-    const { password, ...rest } = this.toJSON();
-    return rest
-}
+  const { password, ...rest } = this.toJSON();
+  return rest;
+};
 
 module.exports = {
-    comparePassword,
-    changePassword,
-    cleanUser
-}
+  comparePassword,
+  changePassword,
+  cleanUser,
+};
